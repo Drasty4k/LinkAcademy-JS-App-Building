25c3ee74308a9d366f399b3e0e171854daeb68ef
DragosDev <barbuta.dragos12@gmail.com>
2021-07-23 21:10:12 +0300

    lecture 6

---
+++ blog/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 8080
+
+function createUser(req, resp) {
+    let email = req.body.email
+    let password = req.body.password
+
+    
+}
+
+app.use(cors())
+server.use(express.json())
+
+app.post('/api/user', createUser)
+
+app.listen(
+    port,
+    () => { console.log(`Listening on http://localhost:${port}`) })
